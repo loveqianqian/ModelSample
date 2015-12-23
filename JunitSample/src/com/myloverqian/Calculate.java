80794ef02c65e973b02a6b965d00509b5c884abe
blogSample <873850653@qq.com>
2015-12-23 12:36:59 +0800

    my test

---
+++ JunitSample/src/com/myloverqian/Calculate.java
@@ -0,0 +1,20 @@
+package com.myloverqian;
+
+/**
+ * Created by dzw on 2015/12/19.
+ */
+public class Calculate {
+    public int add(int... params) {
+        int result = 0;
+        for (int param : params) {
+            result += param;
+        }
+        return result;
+    }
+    public int subtraction(int... params){
+        int result=0;
+
+        return  result;
+    }
+}
+
---
+++ xmlSample/src/main/java/com/myloverqian/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by dzw on 2015/12/23.
+ */
+package com.myloverqian;
